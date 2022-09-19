2cd773eddcff0b4b8ae2db53912a61d3cda509f7
Cintrian <velegoj2@yandex.ru>
2022-09-19 22:09:25 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 10; i>0; i--) {
+        for (int i = 0; i<=17; i=i+2) {
             System.out.println(i);
         }
     }
