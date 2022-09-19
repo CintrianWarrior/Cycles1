91cea599d5eed9baba0c1c65b6598ca2d90cbcff
Cintrian <velegoj2@yandex.ru>
2022-09-19 22:54:15 +0300

    Task3.2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         int monthlyDeposit = 29000;
         int totalDeposit = 0;
         for (int i = 1; i<=12; i++) {
+            totalDeposit = totalDeposit + totalDeposit/100;
             totalDeposit = totalDeposit + monthlyDeposit;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalDeposit + " рублей");
-    }
+        }
     }
 }
