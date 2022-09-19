545e0936ce297656e2ed5b7f0161b158dd657068
Cintrian <velegoj2@yandex.ru>
2022-09-19 22:38:04 +0300

    Task3.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i<=512; i=i*2) {
-            System.out.println(i);
-        }
+        int monthlyDeposit = 29000;
+        int totalDeposit = 0;
+        for (int i = 1; i<=12; i++) {
+            totalDeposit = totalDeposit + monthlyDeposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalDeposit + " рублей");
+    }
     }
 }
